38bdba3790fcd4fe74dd1f707842d2272edb1a53
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-09-29 10:55:54 +0000

    Update about.css and page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -22,6 +22,87 @@ export default function About() {
             </div>
           </div>
         </section>
+        <section id="about-features">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-md-6">
+                <img src="/img/hero_contact.png" alt="" />
+              </div>
+              <div className="col-md-6">
+                <div className="content">
+                  <span>A CHOICE OF PROGRAMS FOR EVERY LEVEL AND NEED</span>
+                  <h3>Our main features</h3>
+                  <p>
+                    Elevate your vector art in the advanced digital Illustration
+                    course at Tabula. Instructor-led creative classes at your
+                    pace. Elevate your vector art in the advanced digital
+                    Illustration course at Tabula. Instructor-led creative
+                    classes at your pace grow the holistic world view of
+                    disruptive.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3">
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-geo-alt"></i>
+                  </div>
+                  <div className="text">
+                    <h4>Location</h4>
+                    <p>
+                      Tote bag trust fund tacos organic four dollar toast
+                      kickstarter pork belly
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-geo-alt"></i>
+                  </div>
+                  <div className="text">
+                    <h4>Location</h4>
+                    <p>
+                      Tote bag trust fund tacos organic four dollar toast
+                      kickstarter pork belly
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-geo-alt"></i>
+                  </div>
+                  <div className="text">
+                    <h4>Location</h4>
+                    <p>
+                      Tote bag trust fund tacos organic four dollar toast
+                      kickstarter pork belly
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-geo-alt"></i>
+                  </div>
+                  <div className="text">
+                    <h4>Location</h4>
+                    <p>
+                      Tote bag trust fund tacos organic four dollar toast
+                      kickstarter pork belly
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
